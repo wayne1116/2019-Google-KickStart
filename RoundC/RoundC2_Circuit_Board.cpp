b34aa0df611ee848ab3c00e41970e4d49cff279e
Wayne Chen <wayne861116@gmail.com>
2019-10-29 20:19:45 +0800

    Update RoundC2_Circuit_Board.cpp

--- RoundC/RoundC2_Circuit_Board.cpp
+++ RoundC/RoundC2_Circuit_Board.cpp
@@ -1,53 +1,49 @@
-#include<stdio.h>
-#include<stdlib.h>
-int min(int a, int b)
-{
-	return a>b?b:a;
-}
-int max(int a, int b)
-{
-	return a>b?a:b;
-}
+#include<iostream>
+#include<algorithm>
+using namespace std;
 int main()
 {
-	int a, i, j, k;
-	int row, col, diff;
+	int a;
+	int row, col, dthick;
+	int board[301][301];
+	int dp[301][301];
 	int Min, Max;
-	int result, index=1;
-	int map[301][301];
-	int temp[301][301];
 	
-	scanf("%d",&a);
-	while(a--){
-		scanf("%d%d%d",&row,&col,&diff);
-		for(i=0; i<row; i++){
-			for(j=0; j<col; j++) scanf("%d",&map[i][j]);
-		} 
+	cin >> a;
+	for(int index=1; index<=a; index++){
+		cin >> row >> col >> dthick;
+		for(int i=0; i<row; i++){
+			for(int j=0; j<col; j++){
+				cin >> board[i][j];
+			}
+		}
 		
-		for(i=0; i<row; i++){                 //A circuit board is good if in each row,
-			for(j=0; j<col; j++){         //the difference between the thickest square 
-				Min=map[i][j];        //and the least thick square is no greater than K.
-				Max=map[i][j];
+		for(int i=0; i<row; i++){
+			for(int j=0; j<col; j++){
+				Min=board[i][j];
+				Max=board[i][j];
+				int k;
 				for(k=j+1; k<col; k++){
-					Min=min(Min,map[i][k]);
-					Max=max(Max,map[i][k]);
-					if(Max-Min>diff) break;
+					Min=min(Min, board[i][k]);
+					Max=max(Max, board[i][k]);
+					if(Max-Min>dthick) break;
 				}
-				temp[i][j]=k-j;
+				dp[i][j]=k-j;
 			}
 		}
 		
-		result=row;
-		for(i=0; i<row; i++){                  // calculate the columns and find a good subcircuit board
-			for(j=0; j<col; j++){
-				Min=temp[i][j];
+		int result=row;
+		for(int i=0; i<row; i++){
+			for(int j=0; j<col; j++){
+				int k;
+				Min=dp[i][j];
 				for(k=i; k<row; k++){
-					Min=min(Min,temp[k][j]);
-					result=max(result, Min*(k-i+1));
-				}	
+					Min=min(Min, dp[k][j]);
+					result=max(result,(k-i+1)*Min);
+				}
 			}
 		}
-		printf("Case #%d: %d\n", index++, result); 
+		cout << "Case #" << index << ": " << result << endl;
 	}
 	return 0;
 }
