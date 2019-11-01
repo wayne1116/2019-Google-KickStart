a142de351fe361ca29b8b332160e7ddef54c60b2
Wayne Chen <wayne861116@gmail.com>
2019-11-01 14:25:46 +0800

    Update Street_Checkers.cpp

--- RoundE/Street_Checkers.cpp
+++ RoundE/Street_Checkers.cpp
@@ -11,10 +11,10 @@ int main()
 	int i, j;
 
 	prime.push_back(2);
-	for(i=3; i<=limit; i+=2){
+	for(i=3; i<=limit; i+=2){                           // find the prime number
 		int limit1=(int)sqrt(i);
 		for(j=3; j<=limit1; j+=2) if(i%j==0) break;
-		if(j>limit1) prime.push_back(i);
+		if(j>limit1) prime.push_back(i);           // store the prime number
 	}
 
 	int a;
@@ -26,7 +26,7 @@ int main()
 		cin >> left >> right;
 		result=0;
 		for(i=left; i<=right; i++){
-			if(i%2==0){
+			if(i%2==0){                              // three sub case
 				int temp=i;
 				int count=0;
 				while(temp%2==0){
@@ -37,7 +37,7 @@ int main()
 				if(i==8) ++result;
 				else if(count==2){
 					bool flag=true;
-                                	for(j=0; j<prime.size(); j++){
+                                	for(j=0; j<prime.size(); j++){     // determine whether temp is the prime number
                                         	if(temp<=prime[j]) break;
                                         	else if(temp%prime[j]==0){
                                                 	flag=false;
@@ -50,7 +50,7 @@ int main()
 			}
 			else{
 				bool flag=true;
-				for(j=0; j<prime.size(); j++){
+				for(j=0; j<prime.size(); j++){     // determine whether i is the prime number
 					if(i<=prime[j]) break;
 					else if(i%prime[j]==0){
 						flag=false;
