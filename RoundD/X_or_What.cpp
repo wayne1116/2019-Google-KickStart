b226f2479f126c9731036ecbffdae060bd973bac
Wayne Chen <wayne861116@gmail.com>
2019-12-19 17:21:51 +0800

    Update X_or_What.cpp

--- RoundD/X_or_What.cpp
+++ RoundD/X_or_What.cpp
@@ -1,58 +1,62 @@
+#include<iostream>
+#include<map>
 #include<vector>
-#include<algorithm>
-#include<set>
 using namespace std;
-bool biteven(int a)
+
+bool check_binary(int num)
 {
-	  int count=0;
-	  for(int i=0; i<10; i++){
-		    if(a&1) ++count;
-		    a=a>>1;
-	  }
-  	return count&1==1?false:true;
+    int count=0;
+    while(num>0){
+        count=count+(num&1);
+        num>>=1;
+    }
+    return (count%2==1)?true:false;
 }
 
 int main()
 {
-	  int a, n, q;
-	  int pos, value;
-	  int number;
+    int a;
+    int index;
+    int in;
+    int n, m;
+    int number;
+    map<int,int> oddindex;
+    vector<int> result;
 
-	  cin >> a;
-	  for(int index=1; index<=a; index++){
-		    cin >> n >> q;
-		
-		  set<int> oddlocation;
-		  for(int i=0; i<n; i++){
-			    cin >> number;
-			    if(!biteven(number)) oddlocation.insert(i);			
-		  }
-		
-		  vector<int> result;
-		  while(q--){
-			    cin >> pos >> value;
+    cin >> a;
+    for(int index=1; index<=a; index++){
+        cin >> n >> m;
+        oddindex.clear();
+        for(int i=0; i<n; i++){
+            cin >> number;
+            // check whether the number is xor-even. If not, put it in. 
+            if(check_binary(number)) oddindex[i]=number;
+        }
 
-			    auto iter=oddlocation.find(pos);
-			    bool temp=biteven(value);
-			    if(iter!=oddlocation.end() && temp) oddlocation.erase(iter);
-		      	    else if(iter==oddlocation.end() && !temp) oddlocation.insert(pos);	
+        result.clear();
+        for(int i=0; i<m; i++){
+            cin >> in >> number;
+            // check whether the number is xor-even.
+            bool flag=check_binary(number);
+            // if the number is xor_even
+            if(oddindex.find(in)!=oddindex.end()&&!flag) oddindex.erase(in);   
+	    // if the number is not xor-even
+            else if(oddindex.find(in)==oddindex.end()&&flag || oddindex.find(in)!=oddindex.end()) oddindex[in]=number;
+			
+	    // if the size of set is even             
+            if(oddindex.size()%2==0) result.push_back(n);
+            else{
+                auto iterend=oddindex.end();
+                auto iterbeg=oddindex.begin();
+                --iterend;
+                //  a1, a2, b1, ......, bm, ac, a(c+1); bi in the set for i=1,...,m
+                result.push_back(max(iterend->first, n-iterbeg->first-1));
+            }
+        }
 
-			    if(oddlocation.size()%2==0) result.push_back(n);
-			    else{
-				      auto temp1=oddlocation.begin();
-				      auto temp2=oddlocation.end(); --temp2;
-
-				      int Max=0;
-				      //Max=max(Max, *temp1);
-				      Max=max(Max, n-*temp1-1);
-				      Max=max(Max, *temp2);
-				      //Max=max(Max, n-*temp2-1);
-				      result.push_back(Max);
-			    }
-		  }
-		  cout << "Case #" << index << ":";
-		  for(int i=0; i<result.size(); i++) cout << " " << result[i];
-		  cout << endl;
-	}
-	return 0;
+        cout << "Case #" << index << ":";
+        for(int i=0; i<result.size(); i++) cout << " " << result[i];
+        cout << endl;
+    }
+    return 0;
 }
